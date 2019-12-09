3124b897f598e75d299d8a63c2c061d59b467a34
PedroKautzmann <pedrokautzmann1@gmail.com>
2019-12-09 09:08:22 -0300

    Adjustment

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
-const { MessageEmbed } = require('discord.js');
-const { CommandoClient } = require('discord.js-commando');
+const {
+  MessageEmbed
+} = require('discord.js');
+const {
+  CommandoClient
+} = require('discord.js-commando');
 const Youtube = require('simple-youtube-api');
-const { token, youtubeApiKey } = require('./config.json');
+const {
+  token,
+  youtubeApiKey
+} = require('./config.json');
 const path = require('path');
 const ffmpeg = require('ffmpeg');
 const ytdl = require('ytdl-core');
@@ -9,35 +16,30 @@ const youtube = new Youtube(youtubeApiKey);
 
 
 const client = new CommandoClient({
-    commandPrefix: '!',   
-    disableEveryone: true
+  commandPrefix: '!',
+  disableEveryone: true
 });
 
 client.registry
-    
-    // Registers your custom command groups
-    .registerGroups([
-        ['music', 'Music commands'],
-        ['weather', 'Weather commands']       
-    ])
 
-    // Registers all built-in groups, commands, and argument types
-    .registerDefaults()
+  // Registers your custom command groups
+  .registerGroups([
+    ['music', 'Music commands'],
+    ['weather', 'Weather commands']
+  ])
 
-    // Registers all of your commands in the ./commands/ directory
-    .registerCommandsIn(path.join(__dirname, 'commands'));
+  // Registers all built-in groups, commands, and argument types
+  .registerDefaults()
+
+  // Registers all of your commands in the ./commands/ directory
+  .registerCommandsIn(path.join(__dirname, 'commands'));
 
 global.servers = {};
 
 
 client.on('ready', () => {
   console.log('This bot is online!');
+});
 
-
-    //token conta prefeitura "token": "NjEwNDkyNzYyMzgyNTMyODIy.XVaPZw.VumVccMHIOFUoBmIT7Df0CP1L_c",
-  
-    
-  });
-  
 
 client.login(token);
